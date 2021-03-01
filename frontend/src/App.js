bb276ea863d471725dfa7a8a1705b2df4ac99c05
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-01 17:20:38 -0600

    Import router and Header and implement header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-
+import Header from "./components/header.component"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>hey</p>
-      </header>
+      <Router>
+      <Header className="Header"/>
+      </Router>
+      
     </div>
   );
 }
