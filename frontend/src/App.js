d6908c9fcb4af9572855ad0b94ed6a351d9de67a
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-01 19:04:38 -0600

    Create a Route for all the links They display the correct content when clicked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import Header from "./components/header.component"
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import Contactus from "./components/contact.component"
+import Home from "./components/home.component"
+import Services from "./components/services.component"
+import Aboutus from "./components/about.component"
+import Location from "./components/location.component"
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Header className="Header"/>
+        <Header className="Header"/>
+        <Route path="/" exact component={Home}/>
+        <Route path="/contactus" exact component={Contactus}/>
+        <Route path="/services" exact component={Services}/>
+        <Route path="/aboutus" exact component={Aboutus}/>
+        <Route path="/location" exact component={Location}/>
+
       </Router>
       
     </div>
