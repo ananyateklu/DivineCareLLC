50a40cefa505cac69295cde216227e741379ef97
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-01 19:05:15 -0600

    The links work correctly now

--- frontend/src/components/header.component.jsx
+++ frontend/src/components/header.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../divinelogo.png';
+import logo from '../assets/divinelogo.png';
 import { Link } from 'react-router-dom';
 import './components.css';
 export default class header extends Component {
@@ -8,11 +8,11 @@ export default class header extends Component {
             <header>
                 <img className="Logo" src={logo} alt="logo"/>
                 <div className="Links">
-                    <Link>HOME</Link>
-                    <Link>ABOUT US</Link>
-                    <Link>OUR SERVICES</Link>
-                    <Link>CONTACT US</Link>
-                    <Link>LOCATION</Link>
+                    <Link to="/">HOME</Link>
+                    <Link to="/aboutus">ABOUT US</Link>
+                    <Link to="/services">OUR SERVICES</Link>
+                    <Link to="/contactus">CONTACT US</Link>
+                    <Link to="/location">LOCATION</Link>
                     <button>APPLY NOW</button>
                 </div>
              
