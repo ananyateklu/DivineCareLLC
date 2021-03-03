a3b46f2cc408a3d3ca94017bc7a07d3e666a039e
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-03 16:31:45 -0600

    Create a state for changing the background color Links can now change bg-color when they are open

--- frontend/src/components/header.component.jsx
+++ frontend/src/components/header.component.jsx
@@ -3,16 +3,29 @@ import logo from '../assets/divinelogo.png';
 import { Link } from 'react-router-dom';
 import './components.css';
 export default class header extends Component {
+    state = {
+            ishome: false,
+            isabout: false,
+            isservices: false,
+            iscontact: false,
+            islocation: false,
+        }
+    componentDidMount() {
+        this.setState({ishome: true})
+    }
+
     render() {
+        console.log(this.state.ishome)
         return (
+            
             <header>
                 <img className="Logo" src={logo} alt="logo"/>
                 <div className="Links">
-                    <Link to="/">HOME</Link>
-                    <Link to="/aboutus">ABOUT US</Link>
-                    <Link to="/services">OUR SERVICES</Link>
-                    <Link to="/contactus">CONTACT US</Link>
-                    <Link to="/location">LOCATION</Link>
+                    <Link to="/" className={this.state.ishome ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: true, isabout: false, iscontact: false, isservices: false, islocation: false}) }>HOME</Link>
+                    <Link to="/aboutus" className= {this.state.isabout ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: false , isabout: true, iscontact: false, isservices: false, islocation: false})}>ABOUT US</Link>
+                    <Link to="/services" className= {this.state.isservices ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: false , isabout: false, iscontact: false, isservices: true, islocation: false}) }>OUR SERVICES</Link>
+                    <Link to="/contactus" className= {this.state.iscontact ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: false , isabout: false, iscontact: true, isservices: false, islocation: false}) }>CONTACT US</Link>
+                    <Link to="/location" className= {this.state.islocation ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: false , isabout: false, iscontact: false, isservices: false, islocation: true}) }>LOCATION</Link>
                     <button>APPLY NOW</button>
                 </div>
              
