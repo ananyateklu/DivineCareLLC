0ac10aff35a1741a7d2e856df0341c2ed43c6402
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-03 16:32:25 -0600

    Refactoring home page

--- frontend/src/components/home.component.jsx
+++ frontend/src/components/home.component.jsx
@@ -9,19 +9,22 @@ export default class home extends Component {
             <div className="Home">
                 <img className="Pic1" src={Pic1} alt="pic1"/>
                 <div className="Homewelcome">
-                <h3>WELCOME TO DIVINE CARE LLC</h3>
-                <h6>When it comes to healthcare assistance, 
-                    it is essential to make sure that the person seeking
-                    assistance receives holistic care. Divine Care LLC is
-                    primarily engaged inproviding personalized medical assistance
-                    such as skilled nursing, home care aide and therapeutic services 
-                    through person-centered assistance living for people of all ages.</h6>
-                <h6>Aging adults and senior residents require special healthcare 
-                    assistance that should focus not only on physical and mental 
-                    health but also on their spiritual and social well-being.
-                    At Divine Care LLC, we not only aim to optimize the health
-                    of our client but also encourage them to engage 
-                    within the community and be independent. </h6>
+                    <div className="Titlediv">
+                        <h3 className="Title">WELCOME TO</h3>
+                        <h3 className="Title2"> DIVINE CARE LLC</h3> 
+                    </div>
+                    <h6>When it comes to healthcare assistance, 
+                        it is essential to make sure that the person seeking
+                        assistance receives holistic care. Divine Care LLC is
+                        primarily engaged inproviding personalized medical assistance
+                        such as skilled nursing, home care aide and therapeutic services 
+                        through person-centered assistance living for people of all ages.</h6>
+                        <h6>Aging adults and senior residents require special healthcare 
+                        assistance that should focus not only on physical and mental 
+                        health but also on their spiritual and social well-being.
+                        At Divine Care LLC, we not only aim to optimize the health
+                        of our client but also encourage them to engage 
+                        within the community and be independent. </h6>
                 </div>
                 <div className="Homeservices">
                     <img className="Homeskillpic" src={Pic2} alt="pic2"/>
