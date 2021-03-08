08cf6c09872773e31865de77e495af3050fd0111
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-08 09:17:47 -0600

    Add all content for about page close #2

--- frontend/src/components/about.component.jsx
+++ frontend/src/components/about.component.jsx
@@ -9,7 +9,18 @@ export default class about extends Component {
                     <img src= {Aboutpic} alt="Homecare"/>
                 </div>
                 <div className="Abouttext">
+                    <h3>ABOUT DIVINE CARE LLC</h3>
+                    <h6>Divine Care LLC is a care provider licensed with the state of Minnesota. 
+                        We have a staff of highly trained nurses, home-health aides and certified 
+                        nursing assistants who assist you through every step on your road to 
+                        optimize your health while providing quality medical service and attentive support.</h6>
 
+                        <h6>Our customized person-centered living facility is equipped to accommodate four clients per house, 
+                        with one staff member available for every two clients. We also provide services pertaining to complex 
+                        medical assistance.</h6>
+                        <h6>From a state housed child foster care center in 2012, we have now equipped ourselves to a full-service 
+                        customized adult living facility providing all kind of services from high-health needs, mechanical 
+                        transfers to specialty assistance with day to day chores.</h6>
                 </div>
               
             </div>
