a21de2fba31f061f3055919d1b424eaac209962c
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-09 10:41:50 -0600

    Create google maps component that displays a map

---
+++ frontend/src/components/googlemaps.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+// ...
+
+export class MapContainer extends React.Component {
+
+  render() {
+    const style = {
+        width: '300px',
+        height: '300px',
+        borderRadius: '20px',
+        border: "solid 10px #7daaf3"
+      }
+    return (
+      <Map google={this.props.google} zoom={14} style={style} initialCenter={{lat: 44.71562073515013, lng: -93.24792898688428}}>
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+          
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyDitWuT1uqJiTaMVbVy6QFefw4FUDaNJ5U")
+})(MapContainer);
