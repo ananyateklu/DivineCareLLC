637eafe190f12287d7bb84ad579474d995463da2
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-11 10:57:45 -0600

    Create Locations page and added content

--- frontend/src/components/location.component.jsx
+++ frontend/src/components/location.component.jsx
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import Applevalley from "../assets/applevalley.jpg";
+import Burnsville from "../assets/burnsville.jpg";
 export default class location extends Component {
-    render() {
-        return (
-            <div>
-                <h3>location</h3>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Location">
+        <img
+          alt="Location-icon"
+          src="https://img.icons8.com/officel/100/000000/worldwide-location--v2.png"
+        />
+        <h3>OUR LOCATIONS</h3>
+        <div className="Locationpics">
+          <div className="Locationdiv1">
+            <img alt="location-pic" src={Applevalley} />
+          </div>
+          <div className="Locationdiv2">
+            <img alt="location-pic" src={Burnsville} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
