b49305464c18e848bcad5329af69c46757746085
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-14 16:26:35 -0500

    Add more content to locations page

--- frontend/src/components/location.component.jsx
+++ frontend/src/components/location.component.jsx
@@ -10,12 +10,26 @@ export default class location extends Component {
           src="https://img.icons8.com/officel/100/000000/worldwide-location--v2.png"
         />
         <h3>OUR LOCATIONS</h3>
+
         <div className="Locationpics">
-          <div className="Locationdiv1">
-            <img alt="location-pic" src={Applevalley} />
+          <div className="location1">
+            <div className="Locationpic1">
+              <img alt="location-pic" src={Applevalley} />
+            </div>
+            <div className="Locationtext1">
+              <h6>2606 River Hills Drive, Burnsville MN 55337</h6>
+              <h6>(952) 977-9171</h6>
+            </div>
           </div>
-          <div className="Locationdiv2">
-            <img alt="location-pic" src={Burnsville} />
+          <div className="location2">
+            <div className="Locationpic2">
+              <img alt="location-pic" src={Burnsville} />
+            </div>
+            <div className="Locationtext2">
+              <h6>14135 Garrett Ave, Apple Valley, MN 55124</h6>
+              <h6>(952) 977-9171</h6>
+            </div>
+            
           </div>
         </div>
       </div>
