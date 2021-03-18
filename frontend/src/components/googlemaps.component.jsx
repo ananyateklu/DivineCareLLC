85728d8d2ce070b114178632c2dd46491d4deaef
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-18 15:38:19 -0500

    Update google maps component to have the api key There is a styling change as well

--- frontend/src/components/googlemaps.component.jsx
+++ frontend/src/components/googlemaps.component.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import "./components.css";
+require("dotenv").config();
 // ...
 
 export class MapContainer extends React.Component {
 
+
   render() {
     const style = {
-        width: '300px',
-        height: '300px',
-        borderRadius: '20px',
-        border: 'solid 10px #7daaf3'
-      }
-    return (
-      <Map google={this.props.google} zoom={14} style={style} initialCenter={{lat: 44.71562073515013, lng: -93.24792898688428}}>
-        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+      zIndex: "0",
+      width: "340px",
+      height: "390px",
+      marginTop: "720px",
+      marginLeft: "650px",
+      borderRadius: "10px",
+      border: "solid 2px #7daaf3",
+    };
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          
-        </InfoWindow>
-      </Map>
+    console.log(process.env);
+
+    return (
+      <div className="MapComponent">
+        <Map
+          className="map"
+          InfoWindow="googlemaps"
+          google={this.props.google}
+          zoom={14}
+          style={style}
+          initialCenter={{ lat: 44.71562073515013, lng: -93.24792898688428 }}
+        >
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+          <InfoWindow onClose={this.onInfoWindowClose}></InfoWindow>
+        </Map>
+      </div>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: ("")
+  apiKey: process.env.REACT_APP_API_KEY,
 })(MapContainer);
