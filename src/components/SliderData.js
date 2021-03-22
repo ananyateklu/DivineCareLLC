2d1a49577fb34affcba1bbf07897eadc2b0925a6
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-22 11:33:08 -0500

    Change the directory of the pictures and add 4th

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,6 +1,6 @@
-import Pic1 from '../assets/homepic1.jpg'
-import Pic2 from '../assets/homepic2.jpg'
-import Pic3 from '../assets/homepic3.jpg'
+import Pic1 from '../assets/Home1.jpg'
+import Pic2 from '../assets/Home2.jpg'
+import Pic3 from '../assets/Home3.jpg'
 
 export const SliderData = [
   {
