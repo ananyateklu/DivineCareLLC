cb4f9d793721ab2aecfd5cf10b4a82533db77d4e
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-22 16:22:05 -0500

    Add burger button for mobile view this displays the links from the top

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 import logo from "../assets/divinelogom.png";
 import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi"
 import "./components.css";
+import { Link } from 'react-router-dom';
 export default class header extends Component {
+  
+  state = {
+    mobile: true
+  }
+
+  expandNav() {
+    this.setState({mobile: !this.state.mobile})
+  }
+  
   render() {
+    const tab_class = this.state.mobile ? "none" : "block";
+
     return (
       <div className="Header">
         <header>
           <img className="Logo" src={logo} alt="logo" />
+          <div className="Burger"><GiHamburgerMenu onClick={() => this.expandNav()} className="Bur"/></div>
+          <div className="Burgerstuff" style={{display : tab_class}}>
+                    <Link className="link" to="/">HOME</Link>
+                    <Link className="link" to="/aboutus">ABOUT US</Link>
+                    <Link className="link" to="/services">OUR SERVICES</Link>
+                    <Link className="link" to="/contactus">CONTACT US</Link>
+                    <Link className="link" to="/location">LOCATION</Link>
+                    <button className="Footerbutton">APPLY NOW</button>    
+                    </div>
           <div className="Links">
             <NavLink
               activeStyle={{
