8c3b7304b0ee94c4bfa040e9699a777a465bcf21
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-22 09:57:58 -0500

    Fix the bug where reloading page messes up navbar close #35

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -1,36 +1,80 @@
-import React, { Component } from 'react';
-import logo from '../assets/divinelogom.png';
-import { Link } from 'react-router-dom';
-import './components.css';
+import React, { Component } from "react";
+import logo from "../assets/divinelogom.png";
+import { NavLink } from "react-router-dom";
+import "./components.css";
 export default class header extends Component {
-    state = {
-            ishome: false,
-            isabout: false,
-            isservices: false,
-            iscontact: false,
-            islocation: false,
-        }
-    componentDidMount() {
-        this.setState({ishome: !this.state.ishome})
-    }
-
-    render() {
-        return (
-            <div className="Header">
-                <header>
-                <img className="Logo" src={logo} alt="logo"/>
-                <div className="Links">
-                    <Link to="/" className={this.state.ishome ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: true, isabout: false, iscontact: false, isservices: false, islocation: false}) }>HOME</Link>
-                    <Link to="/aboutus" className= {this.state.isabout ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: false , isabout: true, iscontact: false, isservices: false, islocation: false})}>ABOUT US</Link>
-                    <Link to="/services" className= {this.state.isservices ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: false , isabout: false, iscontact: false, isservices: true, islocation: false}) }>OUR SERVICES</Link>
-                    <Link to="/contactus" className= {this.state.iscontact ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: false , isabout: false, iscontact: true, isservices: false, islocation: false}) }>CONTACT US</Link>
-                    <Link to="/location" className= {this.state.islocation ? "linkon" : "linkoff"} onClick={ () => this.setState({ishome: false , isabout: false, iscontact: false, isservices: false, islocation: true}) }>LOCATION</Link>
-                    <button>APPLY NOW</button>
-                </div>
-             
-            </header>
-            </div>
-            
-        )
-    }
+  render() {
+    return (
+      <div className="Header">
+        <header>
+          <img className="Logo" src={logo} alt="logo" />
+          <div className="Links">
+            <NavLink
+              activeStyle={{
+                color: "white",
+                backgroundColor: "#5893d4",
+                paddingTop: "40px",
+                paddingBottom: "40px",
+              }}
+              exact
+              to="/"
+              className="linkoff"
+            >
+              HOME
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                color: "white",
+                backgroundColor: "#5893d4",
+                paddingTop: "40px",
+                paddingBottom: "40px",
+              }}
+              to="/aboutus"
+              className="linkoff"
+            >
+              ABOUT US
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                color: "white",
+                backgroundColor: "#5893d4",
+                paddingTop: "40px",
+                paddingBottom: "40px",
+              }}
+              to="/services"
+              className="linkoff"
+            >
+              OUR SERVICES
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                color: "white",
+                backgroundColor: "#5893d4",
+                paddingTop: "40px",
+                paddingBottom: "40px",
+              }}
+              to="/contactus"
+              className="linkoff"
+            >
+              CONTACT US
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                color: "white",
+                backgroundColor: "#5893d4",
+                paddingTop: "40px",
+                paddingBottom: "40px",
+              }}
+              to="/location"
+              className="linkoff"
+            >
+              LOCATION
+            </NavLink>
+            <button>APPLY NOW</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
+
