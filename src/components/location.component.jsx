0c11f20b2997508d5e5f6310b90f8412c4318c87
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-30 20:21:17 -0500

    Finalize locations page

--- src/components/location.component.jsx
+++ src/components/location.component.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import Applevalley from "../assets/applevalley.jpg";
 import Burnsville from "../assets/burnsville.jpg";
+import Locationpic from "../assets/extra3.jpg";
 export default class location extends Component {
-
   componentDidMount() {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
   }
-  
+
   render() {
     return (
       <div className="Location">
+        <div className="Locationpic">
+          <img src={Locationpic} alt="location" />
+        </div>
+
         <img
           alt="Location-icon"
           src="https://img.icons8.com/officel/100/000000/worldwide-location--v2.png"
@@ -22,6 +26,7 @@ export default class location extends Component {
               <img alt="location-pic" src={Applevalley} />
             </div>
             <div className="Locationtext1">
+              <h6>Burnsville Site</h6>
               <h6>2606 River Hills Drive, Burnsville MN 55337</h6>
               <h6>(952) 977-9171</h6>
             </div>
@@ -31,10 +36,10 @@ export default class location extends Component {
               <img alt="location-pic" src={Burnsville} />
             </div>
             <div className="Locationtext2">
+              <h6>Apple Valley Site</h6>
               <h6>14135 Garrett Ave, Apple Valley, MN 55124</h6>
               <h6>(952) 977-9171</h6>
             </div>
-            
           </div>
         </div>
       </div>
