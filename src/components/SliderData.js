20f4df6d46ae9c39b173ad89eee1b369e86566a6
Ananya Teklu <teklu009@morris.umn.edu>
2021-03-30 20:22:16 -0500

    Add picture to SliderData

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,6 +1,7 @@
 import Pic1 from '../assets/Home1.jpg'
 import Pic2 from '../assets/Home2.jpg'
 import Pic3 from '../assets/Home3.jpg'
+import Pic4 from '../assets/Home4.jpg'
 
 export const SliderData = [
   {
@@ -14,5 +15,9 @@ export const SliderData = [
   {
     image:
     Pic3
+  },
+  {
+    image:
+    Pic4
   }
 ];
