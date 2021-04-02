a66d437c311b7f83a6e883a803bad8d8052f175c
Ananya Teklu <teklu009@morris.umn.edu>
2021-04-02 13:52:18 -0500

    Locations page has the right information now close #5

--- src/components/location.component.jsx
+++ src/components/location.component.jsx
@@ -27,9 +27,10 @@ export default class location extends Component {
               <img alt="location-pic" src={Applevalley} />
             </div>
             <div className="Locationtext1">
-              <h6>Burnsville Site</h6>
-              <h6>2606 River Hills Drive, Burnsville MN 55337</h6>
-              <h6>(952) 977-9171</h6>
+              <h6>Site: Burnsville</h6>
+              <h6>Address: 2606 River Hills Drive, Burnsville MN 55337</h6>
+              <h6>Phone: (952) 681-2535</h6>
+              <h6>Fax: (952) 681-2410</h6>
             </div>
           </div>
           <div className="location2">
@@ -37,9 +38,10 @@ export default class location extends Component {
               <img alt="location-pic" src={Burnsville} />
             </div>
             <div className="Locationtext2">
-              <h6>Apple Valley Site</h6>
-              <h6>14135 Garrett Ave, Apple Valley, MN 55124</h6>
-              <h6>(952) 977-9171</h6>
+              <h6>Site: Apple Valley</h6>
+              <h6>Address: 14135 Garrett Ave, Apple Valley, MN 55124</h6>
+              <h6>Phone: (952) 683-1914</h6>
+              <h6>Fax: (952) 683-1981</h6>
             </div>
           </div>
         </div>
