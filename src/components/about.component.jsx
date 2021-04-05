28a5689a51fe289ab7de063b48c6257e2cb12c5e
Ananya Teklu <teklu009@morris.umn.edu>
2021-04-05 15:28:52 -0500

    Formatting change and add new pics at the bottom

--- src/components/about.component.jsx
+++ src/components/about.component.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import Aboutpic from "../assets/aboutuspic12.jpg";
+import New1 from "../assets/new1.jpg";
+import New2 from "../assets/new2.jpg";
+import New3 from "../assets/new3.jpg";
+import New4 from "../assets/new4.jpg";
 
 export default class about extends Component {
   componentDidMount() {
@@ -12,62 +16,70 @@ export default class about extends Component {
           <img src={Aboutpic} alt="Homecare" />
         </div>
         <div className="Abouttext">
-          <img
-            alt="about"
-            src="https://img.icons8.com/dusk/128/000000/about.png"
-          />
-          <div className="Titlediv">
-            <h3 className="Title">ABOUT</h3>
-            <h3 className="Title2"> DIVINE CARE LLC</h3>
+          <div className="AboutContent">
+            <img
+              className="Abouticon"
+              alt="about"
+              src="https://img.icons8.com/dusk/128/000000/about.png"
+            />
+            <div className="Titlediv">
+              <h3 className="Title">ABOUT</h3>
+              <h3 className="Title2"> DIVINE CARE LLC</h3>
+            </div>
+            {/* <h3>ABOUT DIVINE CARE LLC</h3> */}
+            <h6>
+              Divine Care LLC is a care provider licensed with the state of
+              Minnesota. We have a staff of highly trained nurses, home-health
+              aides and certified nursing assistants who assist you through
+              every step on your road to optimize your health while providing
+              quality medical service and attentive support.
+            </h6>
+            <h6>
+              Our customized person-centered living facility is equipped to
+              accommodate four clients per house, with one staff member
+              available for every two clients. We also provide services
+              pertaining to complex medical assistance.
+            </h6>
+            <h6>
+              From a state housed child foster care center in 2012, we have now
+              equipped ourselves to a full-service customized adult living
+              facility providing all kind of services from high-health needs,
+              mechanical transfers to specialty assistance with day to day
+              chores.
+            </h6>
+            <br />
+            <br />
+            <br />
+            <br />
+            <h3 className="Title">Meet </h3>{" "}
+            <h3 className="Title2"> Fentu Zewdie, RN</h3>
+            <h6>
+              Working as a nurse since 1988, Fentu Zewdie, RN holds over 26
+              years of experience in hospital settings and intensive care unit
+              (ICU). Over these years, she was associated with coveted medical
+              institutions like MD Anderson Cancer Centre in Texas and the Abbot
+              North-Western Hospital in Minnesota. Married, with four grown
+              kids, Fentu is a follower of the Christian faith.{" "}
+            </h6>
+            <h6>
+              From being a floor nurse to now owning and managing an customized
+              living facility, Fentu has come a long way. Her mission behind the
+              establishment of Divine CareLLC is to move the people of Minnesota
+              from nursing home to a home-based care environment where they can
+              live within the community and maintain optimal health.{" "}
+            </h6>
+            <h6>
+              Fentu has always been passionate about seeing people grow and
+              helping them turn their life around. For her,its not just about
+              providing healthcare assistance or aiding residents in their day
+              to day tasks like feeding or cleaning; she wants everyone to
+              strive in the community an cherish their independence.
+            </h6>
           </div>
-          {/* <h3>ABOUT DIVINE CARE LLC</h3> */}
-          <h6>
-            Divine Care LLC is a care provider licensed with the state of
-            Minnesota. We have a staff of highly trained nurses, home-health
-            aides and certified nursing assistants who assist you through every
-            step on your road to optimize your health while providing quality
-            medical service and attentive support.
-          </h6>
-          <h6>
-            Our customized person-centered living facility is equipped to
-            accommodate four clients per house, with one staff member available
-            for every two clients. We also provide services pertaining to
-            complex medical assistance.
-          </h6>
-          <h6>
-            From a state housed child foster care center in 2012, we have now
-            equipped ourselves to a full-service customized adult living
-            facility providing all kind of services from high-health needs,
-            mechanical transfers to specialty assistance with day to day chores.
-          </h6>
-          <br />
-          <br />
-          <br />
-          <br />
-          <h3 className="Title">Meet </h3>{" "}
-          <h3 className="Title2"> Fentu Zewdie, RN</h3>
-          <h6>
-            Working as a nurse since 1988, Fentu Zewdie, RN holds over 26 years
-            of experience in hospital settings and intensive care unit (ICU).
-            Over these years, she was associated with coveted medical
-            institutions like MD Anderson Cancer Centre in Texas and the Abbot
-            North-Western Hospital in Minnesota. Married, with four grown kids,
-            Fentu is a follower of the Christian faith.{" "}
-          </h6>
-          <h6>
-            From being a floor nurse to now owning and managing an customized
-            living facility, Fentu has come a long way. Her mission behind the
-            establishment of Divine CareLLC is to move the people of Minnesota
-            from nursing home to a home-based care environment where they can
-            live within the community and maintain optimal health.{" "}
-          </h6>
-          <h6>
-            Fentu has always been passionate about seeing people grow and
-            helping them turn their life around. For her,its not just about
-            providing healthcare assistance or aiding residents in their day to
-            day tasks like feeding or cleaning; she wants everyone to strive in
-            the community an cherish their independence.
-          </h6>
+          <img className="BottomPics" src={New1} alt="" />
+          <img className="BottomPics" src={New2} alt="" />
+          <img className="BottomPics" src={New3} alt="" />
+          <img className="BottomPics" src={New4} alt="" />
         </div>
       </div>
     );
--- src/components/services.component.jsx
+++ src/components/services.component.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import Servicespic from "../assets/homepic2.jpg";
+import New5 from "../assets/new5.jpg";
+import New6 from "../assets/new6.jpg";
+import New12 from "../assets/new12.jpeg";
+import New10 from "../assets/new10.jpeg";
 
 export default class services extends Component {
   componentDidMount() {
@@ -13,72 +17,79 @@ export default class services extends Component {
           <img src={Servicespic} alt="Homecare" />
         </div>
         <div className="Servicestext">
-          <div className="Servicestitle">
-            <img
-              alt="services"
-              src="https://img.icons8.com/dusk/100/000000/treatment-plan.png"
-            />
-            <div className="Titlediv">
-              <h3 className="Title">SERVICES PROVIDED BY</h3>
-              <h3 className="Title2"> DIVINE CARE LLC</h3>
+          <div className="ServicesContent">
+            <div className="Servicestitle">
+              <img
+                alt="services"
+                src="https://img.icons8.com/dusk/100/000000/treatment-plan.png"
+              />
+              <div className="Titlediv">
+                <h3 className="Title">SERVICES PROVIDED BY</h3>
+                <h3 className="Title2"> DIVINE CARE LLC</h3>
+              </div>
+              {/* <h3>SERVICES PROVIDED BY DIVINE CARE LLC</h3> */}
             </div>
-            {/* <h3>SERVICES PROVIDED BY DIVINE CARE LLC</h3> */}
-          </div>
-          <h4>HOME CARE SERVICES</h4>
-          <div className="Homeservices1">
-            <ul>
-              <li>Registered Nurse Services</li>
-              <li>Licenses Practical Nurse Services</li>
-              <li>Medication Management Services</li>
-              <li>
-                Preparing modified diets ordered by licensed health
-                professionals
-              </li>
-              <li>Shopping</li>
-              <li>
-                Providing eating assistance for clients with complicating eating
-                problems (i.e. difficulty swallowing, recurrent lung
-                aspirations, or requiring the use of a tube, parenteral or
-                intravenous instruments)
-              </li>
-              <li>Meal preparations </li>
-            </ul>
-          </div>
-          <div className="Homeservices2">
-            <ul>
-              <li>Hands-on assistance with transfers and mobility</li>
-              <li>LaundryHousekeeping/ Other household chores</li>
+            <h4>HOME CARE SERVICES</h4>
+            <div className="Homeservices1">
+              <ul>
+                <li>Registered Nurse Services</li>
+                <li>Licenses Practical Nurse Services</li>
+                <li>Medication Management Services</li>
+                <li>
+                  Preparing modified diets ordered by licensed health
+                  professionals
+                </li>
+                <li>Shopping</li>
+                <li>
+                  Providing eating assistance for clients with complicating
+                  eating problems (i.e. difficulty swallowing, recurrent lung
+                  aspirations, or requiring the use of a tube, parenteral or
+                  intravenous instruments)
+                </li>
+                <li>Meal preparations </li>
+              </ul>
+            </div>
+            <div className="Homeservices2">
+              <ul>
+                <li>Hands-on assistance with transfers and mobility</li>
+                <li>LaundryHousekeeping/ Other household chores</li>
 
-              <li>Complex or Specialty Care services</li>
-              <li>Delegated tasks to unlicensed personnel</li>
-              <li>
-                Assistance with dressing, self-feeding, oral hygiene, haircare,
-                grooming, toileting, and bathing
-              </li>
-              <li>
-                Providing standby assistance within arm's reach for safety while
-                performing daily activities
-              </li>
-              <li>
-                Providing verbal or visual reminders to the client to perform
-                regularly scheduled treatments and exercises
-              </li>
-            </ul>
+                <li>Complex or Specialty Care services</li>
+                <li>Delegated tasks to unlicensed personnel</li>
+                <li>
+                  Assistance with dressing, self-feeding, oral hygiene,
+                  haircare, grooming, toileting, and bathing
+                </li>
+                <li>
+                  Providing standby assistance within arm's reach for safety
+                  while performing daily activities
+                </li>
+                <li>
+                  Providing verbal or visual reminders to the client to perform
+                  regularly scheduled treatments and exercises
+                </li>
+              </ul>
+            </div>
+            <div className="Payment">
+              <h4>PAYMENT METHOD</h4>
+              <ul>
+                <li>Private Pay </li>
+                <li>Medical Assistance</li>
+                <li>CADI Wavier</li>
+              </ul>
+            </div>
+            <div className="Agegroup">
+              <h4>AGE GROUP</h4>
+              <ul>
+                <li>18 and older</li>
+              </ul>
+            </div>
           </div>
-          <div className="Payment">
-            <h4>PAYMENT METHOD</h4>
-            <ul>
-              <li>Private Pay </li>
-              <li>Medical Assistance</li>
-              <li>CADI Wavier</li>
-            </ul>
-          </div>
-          <div className="Agegroup">
-            <h4>AGE GROUP</h4>
-            <ul>
-              <li>18 and older</li>
-            </ul>
-          </div>
+
+          <img className="BottomPics" src={New5} alt="" />
+          <img className="BottomPics" src={New6} alt="" />
+          <img className="BottomPics" src={New12} alt="" />
+          <img className="BottomPics" src={New10} alt="" />
         </div>
       </div>
     );
